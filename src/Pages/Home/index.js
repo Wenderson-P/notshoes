4cf82227e825a5fdd6af0c2bbeb229881a40f772
Wenderson <wps713@gmail.com>
2019-11-10 17:00:14 -0300

    Add product list in home
    We are not adding images in our database for now, first, we will add some style in this list

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis hike</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }

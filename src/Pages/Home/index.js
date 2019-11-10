001fc53ebfe86fb97c0bf3d0998ebb9e67a6a283
Wenderson <wps713@gmail.com>
2019-11-10 19:17:42 -0300

    Changing static content to api data

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -14,22 +14,22 @@ export default class Home extends Component {
   }
 
   render() {
+    const { products } = this.state;
     return (
       <ProductList>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_zoom1.jpg"
-            alt="Tênis"
-          />
-          <strong>Tênis hike</strong>
-          <span>R$129,90</span>
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" />
-            </div>
-            <span>Adicionar ao carrinho</span>
-          </button>
-        </li>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>R${product.price} </span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" />
+              </div>
+              <span>Adicionar ao carrinho</span>
+            </button>
+          </li>
+        ))}
       </ProductList>
     );
   }

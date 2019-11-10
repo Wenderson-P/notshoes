1bc214ef6fc1fe95e36ac8f3b0f8ea6e0ab8a5e2
Wenderson <wps713@gmail.com>
2019-11-10 15:58:31 -0300

    Add roboto font

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
   *{
     margin: 0;
     padding:0;
@@ -13,7 +14,7 @@ export default createGlobalStyle`
     -webkit-font-smoothing: antialiased;
   }
   body,input,button{
-    font:14px sans-serif;
+    font:14px Roboto,sans-serif;
   }
 
   #root{

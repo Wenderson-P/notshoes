9414d6aba6ac0d6a7de9574eefce5e3de55d6f8d
Wenderson <wps713@gmail.com>
2019-11-10 18:29:43 -0300

    Add cart itens

--- src/Pages/Cart/index.js
+++ src/Pages/Cart/index.js
@@ -1,5 +1,65 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <div />;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/36/HZM-1731-036/HZM-1731-036_zoom2.jpg"
+                alt="Tênis Hike"
+              />
+            </td>
+            <td>
+              <strong>Tênis muito massa </strong>
+              <span>R$329,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={10} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$3290,90</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={14} color="7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>Total</span>
+          <strong>R$1200,35</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }

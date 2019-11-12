55eef4949cd0b28e3a8944ea100bb72825f9dbdc
Wenderson <wps713@gmail.com>
2019-11-11 21:50:33 -0300

    Setting saga in the store file

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './modules/rootSaga';
 import rootReducer from './modules/rootReducer';
 
+const sagaMiddleware = createSagaMiddleware();
+
 const enhacer =
-  process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
+  process.env.NODE_ENV === 'development'
+    ? compose(console.tron.createEnhancer(), applyMiddleware(sagaMiddleware))
+    : applyMiddleware(sagaMiddleware);
 const store = createStore(rootReducer, enhacer);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;

7528b665251900c1f9c834b05595c26a997925f1
Wenderson <wps713@gmail.com>
2019-11-12 08:53:46 -0300

    Add history push to cart when a product is selected

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -2,6 +2,8 @@ import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
+import history from '../../../services/history';
+
 import { formatPrice } from '../../../util/format';
 import { addToCartSucess, updateAmountSuccess } from './actions';
 
@@ -30,6 +32,7 @@ function* addToCart({ id }) {
       priceFormatted: formatPrice(response.data.price),
     };
     yield put(addToCartSucess(data));
+    history.push('/cart');
   }
 }
 

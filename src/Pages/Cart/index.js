649b2b42a4c3b29e919bb9475bd2201b8dc7cd98
Wenderson <wps713@gmail.com>
2019-11-12 09:15:24 -0300

    Add cart empty state

--- src/Pages/Cart/index.js
+++ src/Pages/Cart/index.js
@@ -4,9 +4,10 @@ import {
   MdAddCircleOutline,
   MdDelete,
 } from 'react-icons/md';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Container, ProductTable, Total } from './styles';
+import { Container, ProductTable, Total, EmptyScreen } from './styles';
 import { formatPrice } from '../../util/format';
 import * as CartActions from '../../store/modules/cart/actions';
 
@@ -21,63 +22,76 @@ function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
 
   return (
     <Container>
-      <ProductTable>
-        <thead>
-          <tr>
-            <th />
-            <th>PRODUTO</th>
-            <th>QTD</th>
-            <th>SUBTOTAL</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          {cart.map(product => (
-            <tr>
-              <td>
-                <img src={product.image} alt={product.title} />
-              </td>
-              <td>
-                <strong>{product.title}</strong>
-                <span>{product.priceFormatted}</span>
-              </td>
-              <td>
-                <div>
-                  <button type="button" onClick={() => decrement(product)}>
-                    <MdRemoveCircleOutline size={20} color="#7159c1" />
-                  </button>
-                  <input type="number" value={product.amount} />
-                  <button type="button">
-                    <MdAddCircleOutline
-                      size={20}
-                      color="#7159c1"
-                      onClick={() => increment(product)}
-                    />
-                  </button>
-                </div>
-              </td>
-              <td>
-                <strong>{product.subTotal}</strong>
-              </td>
-              <td>
-                <button
-                  type="button"
-                  onClick={() => removeFromCart(product.id)}
-                >
-                  <MdDelete size={14} color="7159c1" />
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </ProductTable>
-      <footer>
-        <button type="button">Finalizar pedido</button>
-        <Total>
-          <span>Total</span>
-          <strong>{total}</strong>
-        </Total>
-      </footer>
+      {cart.length > 0 ? (
+        <>
+          <ProductTable>
+            <thead>
+              <tr>
+                <th />
+                <th>PRODUTO</th>
+                <th>QTD</th>
+                <th>SUBTOTAL</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map(product => (
+                <tr>
+                  <td>
+                    <img src={product.image} alt={product.title} />
+                  </td>
+                  <td>
+                    <strong>{product.title}</strong>
+                    <span>{product.priceFormatted}</span>
+                  </td>
+                  <td>
+                    <div>
+                      <button type="button" onClick={() => decrement(product)}>
+                        <MdRemoveCircleOutline size={20} color="#7159c1" />
+                      </button>
+                      <input type="number" value={product.amount} />
+                      <button type="button">
+                        <MdAddCircleOutline
+                          size={20}
+                          color="#7159c1"
+                          onClick={() => increment(product)}
+                        />
+                      </button>
+                    </div>
+                  </td>
+                  <td>
+                    <strong>{product.subTotal}</strong>
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      onClick={() => removeFromCart(product.id)}
+                    >
+                      <MdDelete size={14} color="7159c1" />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </ProductTable>
+          <footer>
+            <button type="button">Finalizar pedido</button>
+            <Total>
+              <span>Total</span>
+              <strong>{total}</strong>
+            </Total>
+          </footer>
+        </>
+      ) : (
+        <EmptyScreen>
+          <AiOutlineShoppingCart size={50} color="#5a2d82" />
+          <span>Seu carrinho está vazio :(</span>
+          <span>
+            Que tal navegar pela nossa loja e conhecer nossos produtos ?
+          </span>
+          <button>Voltar para a página inicial</button>
+        </EmptyScreen>
+      )}
     </Container>
   );
 }
